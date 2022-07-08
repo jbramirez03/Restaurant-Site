8ba462dc315438a0805a9a9c62e9e6d34d34790c
temptado <jason1287712@gmail.com>
2022-07-08 14:34:09 -0400

    fixed scroll top reveal script

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ window.addEventListener('scroll', scrollHeader)
 function scrollTop() {
     const scrollTop = document.getElementById('scroll-top');
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if (this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+    if (this.scrollY >= 350) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('scroll-top')
 }
 window.addEventListener('scroll', scrollTop)
 
